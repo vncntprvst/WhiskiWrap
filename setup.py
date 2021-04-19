9bb9cf4acb433c31e6e9b8ed0476844c43a17022
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2021-04-19 14:11:14 +0200

    script to apply whiski detection/trace.
    Step1: trace

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+   name='WhiskiWrap',
+   version='1.0',
+   author='',
+   author_email='',
+   packages=['WhiskiWrap','my'],
+   # scripts=['bin/script1','bin/script2'],
+   url='http://pypi.python.org/pypi/WhiskiWrap/',
+   license='LICENSE.txt',
+   description='whiski package wrapper created by cxrodgers',
+   long_description=open('README.md').read(),
+   install_requires=['tables','pandas','MediaInfo'],
+)
