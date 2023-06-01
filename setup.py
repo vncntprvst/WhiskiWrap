2a7c646c60ded8a1dd0429f633c8e8c9ba49bf3d
Vincent Prevosto <prevosto@mit.edu>
2023-06-01 12:27:12 -0400

    updates setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
    name='WhiskiWrap',
-   version='1.1.1',
-   author='',
+   version='1.1.2',
+   authors=['cxrodgers','aiporre','vncntprvst'],
    author_email='',
    packages=['WhiskiWrap','wwutils'],
    # scripts=['bin/script1','bin/script2'],
    url='http://pypi.python.org/pypi/WhiskiWrap/',
    license='LICENSE.txt',
-   description='whiski package wrapper created by cxrodgers',
+   description='Whisk package wrapper created by cxrodgers',
    long_description=open('README.md').read(),
    install_requires=['tables>=3.5.1','pandas','MediaInfo'],
    include_package_data=True,
