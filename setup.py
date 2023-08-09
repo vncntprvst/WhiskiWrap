bd73b28fa5d6ce2026587be6c4b7ce041c0cf746
Vincent <prevosto@mit.edu>
2023-08-09 10:10:01 -0400

    updates setup.py to create dist

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ from setuptools import setup
 setup(
    name='WhiskiWrap',
    version='1.1.2',
-   authors=['cxrodgers','aiporre','vncntprvst'],
+   author='Chris Rodgers',
    author_email='',
+   maintainer=', '.join(['cxrodgers','aiporre','vncntprvst']),
    packages=['WhiskiWrap','wwutils'],
    # scripts=['bin/script1','bin/script2'],
    url='http://pypi.python.org/pypi/WhiskiWrap/',
    license='LICENSE.txt',
    description='Whisk package wrapper created by cxrodgers',
    long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
    install_requires=[
       'tables>=3.5.1',
       'pandas',
