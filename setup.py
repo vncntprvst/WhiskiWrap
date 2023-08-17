4db5c76095df288cf9204a24baec948d67408015
Vincent Prevosto <prevosto@mit.edu>
2023-08-17 18:46:00 -0400

    v 1.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
    name='WhiskiWrap',
-   version='1.1.2',
+   version='1.1.3',
    author='Chris Rodgers',
    author_email='',
    maintainer=', '.join(['cxrodgers','aiporre','vncntprvst']),
