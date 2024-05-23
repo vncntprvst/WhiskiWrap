2016203e72b0f756b01d63f5dd842d344bdffb35
Vincent <prevosto@mit.edu>
2024-05-23 18:41:27 -0400

    update to v1.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
    name='WhiskiWrap',
-   version='1.1.9',
+   version='1.1.10',
    author='Chris Rodgers',
    author_email='',
    maintainer=', '.join(['cxrodgers','aiporre','vncntprvst']),
