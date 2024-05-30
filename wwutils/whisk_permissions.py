64f6ad784854cc910a8aada83cac05ccbf219832
Vincent <prevosto@mit.edu>
2024-05-30 10:58:56 -0400

    refactor: Update permissions logic to locate 'whisk/bin' directory dynamically

--- wwutils/whisk_permissions.py
+++ wwutils/whisk_permissions.py
@@ -2,18 +2,42 @@
 
 import os
 import stat
-import distutils.sysconfig
+# import distutils.sysconfig
+import sys
+from importlib.metadata import distribution, PackageNotFoundError
+
+def find_whisk_bin_dir():
+    try:
+        # Locate the 'whisk' package directory
+        whisk_dist = distribution("whisk-janelia")
+        whisk_location = whisk_dist.locate_file('')
+        bin_dir = os.path.join(whisk_location, 'whisk', 'bin')
+        
+        # # Locate the 'bin' directory in the site-packages
+        # site_packages_dir = distutils.sysconfig.get_python_lib()
+        # bin_dir = os.path.join(site_packages_dir, 'whisk', 'bin')
+
+        if os.path.exists(bin_dir):
+            return bin_dir
+    except PackageNotFoundError:
+        pass
+
+    # If the package is not found, check for develop mode
+    for path in sys.path:
+        possible_dir = os.path.join(path, 'whisk', 'bin')
+        if os.path.exists(possible_dir):
+            return possible_dir
+
+    return None
 
 def update_permissions(verbose=False):
-    # Locate the 'bin' directory in the site-packages
-    site_packages_dir = distutils.sysconfig.get_python_lib()
-    bin_dir = os.path.join(site_packages_dir, 'whisk', 'bin')
+    bin_dir = find_whisk_bin_dir()
 
-    if not os.path.exists(bin_dir):
-        print("Error: 'bin' directory not found. Please ensure the package is installed correctly.")
+    if not bin_dir:
+        print("Error: 'whisk/bin' directory not found. Please ensure the package is installed correctly.")
         return
-
-    #  Test if trace can be executed
+        
+    # Test if trace can be executed
     trace_file = os.path.join(bin_dir, 'trace')
     if not os.access(trace_file, os.X_OK):
         print("Updating permissions of executable files in the 'whisk/bin' directory")
@@ -39,3 +63,4 @@ def update_permissions(verbose=False):
 
 if __name__ == "__main__":
     update_permissions()
+
